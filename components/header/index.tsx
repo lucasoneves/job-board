e5dc8751022f170d5e249c3a0ba9cb945abc375e
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-04 09:32:49 -0300

    Header component, installed heroicons and tailwind

---
+++ components/header/index.tsx
@@ -0,0 +1,38 @@
+import styles from "./header.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="drop-shadow-md bg-slate-300 p-5">
+      <nav className="container flex items-center justify-between">
+        <h2 className="text-3xl font-bold">job board</h2>
+        <ul className="flex items-center justify-between gap-10">
+          <li>
+            <Link href="/jobs">jobs</Link>
+          </li>
+          <li>
+            <Link href="/new-job" className="flex items-center gap-1 bg-violet-400 px-4 py-2 rounded-lg">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                  />
+                </svg>
+              </span>
+              new job
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
