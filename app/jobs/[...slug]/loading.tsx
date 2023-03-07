624d21e11b35fcfd517eb4d5fd1f4d5c38e45af6
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-07 17:01:59 -0300

    Loading page

---
+++ app/jobs/[...slug]/loading.tsx
@@ -0,0 +1,3 @@
+export default function JobLoading() {
+  return <div>loading...</div>
+}
--- app/jobs/[...slug]/page.tsx
+++ app/jobs/[...slug]/page.tsx
@@ -1,4 +1,19 @@
-export default function jobDetail({params}) {
-  const job = params.slug[0]
-  return <h2>Job Detail: {job}</h2>
+const delay = (time) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(1), time);
+  });
+
+const getData = async (slug) => {
+  const job = await delay(5000);
+  return {slug};
+};
+
+export default async function jobDetail({ params }) {
+  const { slug } = params;
+  const job = await getData(slug);
+  return (
+    <>
+      <h2>{job.slug}</h2>
+    </>
+  );
 }
