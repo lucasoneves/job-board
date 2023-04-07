e5ee6ccf90a2a274ed5c35207e99801b6b66fe0f
lucasoneves <casluhc@gmail.com>
2023-04-07 15:41:41 -0300

    Project card component

---
+++ components/GreetingSkeleton/index.tsx
@@ -0,0 +1,5 @@
+const GreetingSkeleton = () => {
+    return <h2>Loading...</h2>
+}
+
+export default GreetingSkeleton
--- components/Greetings/index.tsx
+++ components/Greetings/index.tsx
@@ -3,9 +3,10 @@ import { getUserFromCookie } from "@/lib/auth";
 import { cookies } from "next/headers";
 // import Button from "./Button";
 // import Card from "./Card";
-// import { delay } from "@/lib/async";
+import { delay } from "@/lib/async";
 
 const getData = async () => {
+  await delay(5000);
   const user = await getUserFromCookie(cookies());
   return user;
 };
@@ -15,16 +16,16 @@ const Greetings = async () => {
 
   return (
     <div className="mb-4">
-        <h1 className="text-3xl text-gray-700 font-bold mb-4">
-          Hello, {user.firstName}!
-        </h1>
-        <h4 className="text-xl text-gray-400">
-          Check your daily tasks and schedule
-        </h4>
+      <h1 className="text-3xl text-gray-700 font-bold mb-4">
+        Hello, {user.firstName}!
+      </h1>
+      <h4 className="text-xl text-gray-400">
+        Check your daily tasks and schedule
+      </h4>
       <div>
         <button>Today's Schedule</button>
       </div>
-      </div>
+    </div>
   );
 };
 
